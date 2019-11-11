734f333e61c938a58cfcf9e1957cf5fd489de9ec
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-11-11 05:22:49 -0600

    WIP: Update for 2_2 and Abaco sync messages

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM sd2e/reactors:python3-edge
 
 # Comment out if not actively developing python-datacatalog
-ARG DATACATALOG_BRANCH=v2.0.0
+ARG DATACATALOG_BRANCH=2_2
 RUN pip uninstall --yes datacatalog || true
 RUN pip3 install --upgrade --no-cache-dir \
     git+https://github.com/SD2E/python-datacatalog.git@${DATACATALOG_BRANCH}
--- reactor.py
+++ reactor.py
@@ -5,10 +5,12 @@ import copy
 from attrdict import AttrDict
 from pprint import pprint
 from jsonschema import ValidationError
-
 from reactors.runtime import Reactor, agaveutils
-from datacatalog.linkedstore.pipeline import PipelineStore
-from datacatalog.linkedstore.pipeline import PipelineCreateFailure, PipelineUpdateFailure
+
+from datacatalog.linkedstores.basestore import strategies
+from datacatalog.linkedstores.pipeline import (PipelineStore,
+                                               PipelineCreateFailure,
+                                               PipelineUpdateFailure)
 
 
 def main():
@@ -23,18 +25,26 @@ def main():
         except Exception:
             pass
 
-    action = None
+    # Allow passed vars to override
+    token = os.environ.get('token', None)
+    uuid = os.environ.get('uuid', None)
+    if uuid:
+        action = os.environ.get('action', 'show')
+    else:
+        action = None
+
     try:
-        for a in ['create', 'update', 'delete']:
+        for a in ['create']:
             try:
                 schema_file = '/schemas/' + a + '.jsonschema'
-                r.validate_message(
-                    m, messageschema=schema_file, permissive=False)
+                r.validate_message(m,
+                                   messageschema=schema_file,
+                                   permissive=False)
                 action = a
                 break
             except Exception as exc:
-                r.logger.debug(
-                    'Validation to "{0}" failed: {1}\n'.format(a, exc))
+                r.logger.debug('Validation to "{0}" failed: {1}\n'.format(
+                    a, exc))
         if action is None:
             raise ValidationError('Message did not match any known schema')
     except Exception as vexc:
@@ -42,47 +52,38 @@ def main():
 
     r.logger.debug('Action selected: {}'.format(action))
 
-    if '__options' in m:
-        # allow override of settings
-        try:
-            options_settings = m.get('__options', {}).get('settings', {})
-            if isinstance(options_settings, dict):
-                options_settings = AttrDict(options_settings)
-            m.pop('__options')
-            r.settings = r.settings + options_settings
-        except Exception as exc:
-            r.on_failure('Failed to handle options', exc)
-
     # Set up Store objects
     pipe_store = PipelineStore(mongodb=r.settings.mongodb)
+
+    if action == 'get':
+        resp = pipe_store.find_one_by_uuid(uuid)
+        r.logger.info(resp)
+        r.on_success('Exists and was printed to execution log')
 
     if action == 'create':
         create_dict = copy.deepcopy(m)
         try:
-            new_pipeline = pipe_store.add_update_document(create_dict)
-            r.on_success('Created pipeline {} with update token {}'.format(
-                new_pipeline['uuid'], new_pipeline['token']))
+            if uuid and token:
+                pipeline = pipe_store.add_update_document(create_dict)
+            else:
+                pipeline = pipe_store.add_update_document(
+                    create_dict,
+                    uuid=uuid,
+                    token=token,
+                    strategy=strategies.REPLACE)
+            r.on_success('Wrote pipeline {}; Update token: {}'.format(
+                pipeline['uuid'], pipeline['token']))
         except Exception as exc:
-            r.on_failure('Create failed', exc)
+            r.on_failure('Write failed', exc)
 
-    if action == 'update':
-        update_dict = m.get('body', {})
+    if action == 'disable':
         try:
-            new_pipeline = pipe_store.update_pipeline(
-                pipeline_uuid=m['uuid'], update_token=m['token'], **update_dict)
-            r.on_success('Updated pipeline {}'.format(
-                new_pipeline['_uuid']))
+            pipe_store.delete_document(uuid, token, force=False)
+            r.on_success('Disabled pipeline {}'.format(m.get('uuid')))
         except Exception as exc:
-            r.on_failure('Update failed', exc)
+            r.on_failure('Disable request failed', exc)
 
-    if action == 'delete':
-        try:
-            pipe_store.delete(m['uuid'], m['token'], m.get('force', False))
-            r.on_success('Deleted pipeline {}'.format(m.get('uuid')))
-        except Exception as exc:
-            r.on_failure('Delete failed', exc)
-
-    if action == 'undelete':
+    if action == 'enable':
         try:
             pipe_store.undelete(m['uuid'], m['token'])
             r.on_success('Un-deleted pipeline {}'.format(m.get('uuid')))
